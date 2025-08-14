8880777fc3e35d6fa9012efaddb4ed7be622b1df
Tuncay2006 <zenixthedark@gmail.com>
2025-08-14 16:47:51 +0000

    wge

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-# IPv4 için sadece TCP 80 numaralı porta gelen bağlantılara izin ver
-sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-echo "Rules updated"
-
-# IPv6 için aynı kural
-sudo ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
-echo "Rules updated (v6)"
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT && iptables -A INPUT -j DROP
