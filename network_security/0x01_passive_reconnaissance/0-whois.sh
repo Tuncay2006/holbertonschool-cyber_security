bd1967f3ae3709ba7735029c6dc6a321424a591a
Tuncay2006 <zenixthedark@gmail.com>
2025-08-19 15:02:30 +0000

    vfd

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk '/^(Registrant|Admin|Tech)/ {key=$1; val=substr($0,length($1)+2); sub(/^[ \t]+|[ \t]+$/,"",key); sub(/^[ \t]+|[ \t]+$/,"",val); print key "," val}' > $1.csv
+whois $1 | awk -F: '/Registrant|Admin|Tech/ {gsub(/^[ \t]+/, "", $2); print $1 "," $2}'
