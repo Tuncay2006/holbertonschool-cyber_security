d9e6753d0d9bc3d1c7b954280f1a2352f292f9f4
Tuncay2006 <zenix@gmail.com>
2025-08-29 15:17:47 -0400

    fvb

--- network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
+++ network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-# 0-arp_scan.sh
-# Discover live hosts in a subnet using ARP scan with nmap
-# Usage: sudo ./0-arp_scan.sh <subnet> (e.g. 192.168.1.0/24)
-# Check if argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: sudo $0 <subnet>"
-    exit 1
-fi
+sudo nmap -sn -PR "$1"
 
-SUBNET=$1
-
-# Run nmap ARP scan (host discovery only, no port scan)
-sudo nmap -sn -PR "$SUBNET"
