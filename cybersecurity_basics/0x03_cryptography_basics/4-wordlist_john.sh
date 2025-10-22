71bed0eab573a3307f39e8cbf9b19bc3ea424cc0
Tuncay2006 <zenixthedark@gmail.com>
2025-10-22 18:31:48 -0400

    dgfdg

---
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+gzip -dc /home/kali/Downloads/rockyou.txt.gz > /tmp/rockyou.txt
+john --wordlist=/tmp/rockyou.txt --format=raw-md5 "$1"
+john --show --format=raw-md5 "$1" | cut -d: -f2 > 4-password.txt
