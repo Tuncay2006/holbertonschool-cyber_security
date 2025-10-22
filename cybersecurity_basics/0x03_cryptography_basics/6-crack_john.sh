4deec0ef9e6dd36a2c043aa391c9342c208ceef1
Tuncay2006 <zenixthedark@gmail.com>
2025-10-22 19:56:56 -0400

    grd

---
+++ cybersecurity_basics/0x03_cryptography_basics/6-crack_john.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+gzip -dc /home/kali/Downloads/rockyou.txt.gz > /tmp/rockyou.txt && john --wordlist=/tmp/rockyou.txt --format=raw-sha256 "$1" && john --show --format=raw-sha256 "$1" | awk -F: 'NR==1{print $2}' > 6-password.txt
