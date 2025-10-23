fd62c1a5db7c13d4d022f4e6899cd47f454a3aa3
Tuncay2006 <zenixthedark@gmail.com>
2025-10-22 20:27:33 -0400

    gjgygjd

--- cybersecurity_basics/0x03_cryptography_basics/8-combination_hashcat.sh
+++ cybersecurity_basics/0x03_cryptography_basics/8-combination_hashcat.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env bash
+#!/bin/bash
 command -v hashcat >/dev/null 2>&1 && hashcat -a 1 --stdout "$1" "$2" || while IFS= read -r w1; do while IFS= read -r w2; do echo "${w1}${w2}"; done < "$2"; done < "$1"
