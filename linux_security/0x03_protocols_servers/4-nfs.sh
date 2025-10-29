64d03140af57dc98f7f5136e4f02424b562a661a
Tuncay2006 <zenix@gmail.com>
2025-10-28 22:33:31 -0400

    uy7it

--- linux_security/0x03_protocols_servers/4-nfs.sh
+++ linux_security/0x03_protocols_servers/4-nfs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "Export list for $1:"; showmount -e "$1" 2>/dev/null | tail -n +2 | awk '{c=($2=="")?"everyone":$2; print $1 " (" c ")"}'
+showmount -e "$1" 2>/dev/null | tail -n +2 | awk '{c=($2=="")?"everyone":$2; print $1 " (" c ")"}'
