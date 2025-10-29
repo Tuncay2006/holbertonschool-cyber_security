d5cd00b84159b7f2a3a6829c25c275f12df1cdc0
Tuncay2006 <zenix@gmail.com>
2025-10-28 23:15:21 -0400

    uy7it

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo iptables -F
-sudo iptables -P INPUT DROP && sudo iptables -A INPUT -i lo -j ACCEPT && sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -P INPUT DROP && sudo iptables -A INPUT -i lo -j ACCEPT && sudo iptables -A INPUT -p tcp --dport ssh -j ACCEPT
