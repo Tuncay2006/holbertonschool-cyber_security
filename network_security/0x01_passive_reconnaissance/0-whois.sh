00db5b6144eee33694c44d93e3781d5d35446f7a
Tuncay2006 <zenixthedark@gmail.com>
2025-10-30 22:01:23 +0400

    Change shebang to use /bin/bash

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env bash
+#!/bin/bash
 whois "$1" | awk -F': ' '/Registrant|Admin|Tech/ {gsub(/^ +| +$/, "", $2); sub(/Street$/,"Street "); sub(/Ext$/,"Ext:"); print $1 "," $2}' > "$1.csv"
