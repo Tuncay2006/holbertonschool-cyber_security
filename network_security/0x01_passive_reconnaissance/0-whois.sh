95514c8d2605d213a73bd9756abb366fa9d928c6
Tuncay2006 <zenix@gmail.com>
2025-10-30 14:58:25 -0400

    dgfrz

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F: '/Registrant|Admin|Tech/ {gsub(/^[ \t]+|[ \t]+$/,"",$2); f=$1; gsub(/^[ \t]+|[ \t]+$/,"",f); if(f~/Street/){a[f]=$2" "}; print f","$2}' > "$1.csv"
+whois "$1" | awk -F: '/Registrant|Admin|Tech/ {gsub(/^[ \t]+|[ \t]+$/,"",$2); f=$1; gsub(/^[ \t]+|[ \t]+$/,"",f); if(f~/Street/){$2=$2" "}; print f","$2}'
