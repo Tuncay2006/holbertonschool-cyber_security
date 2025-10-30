089689322707e1cd29824708614500da48e2c891
Tuncay2006 <zenixthedark@gmail.com>
2025-10-30 22:08:29 +0400

    Refactor whois script for better output formatting

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': ' '/^(Registrant|Admin|Tech) /{gsub(/^ +| +$/,"",$2);if($1~/Street$/)$2=$2" ";if($1~/Ext$/)sub(/Ext$/,"Ext:");print $1","$2}' | sed '${/^$/d}' > "$1.csv"
+whois "$1" | awk -F': ' 'BEGIN{ORS=""} /^Registrant |^Admin |^Tech /{gsub(/^ +| +$/,"",$2); if($1~/Street$/)$2=$2" "; if($1~/Ext$/)sub(/Ext$/,"Ext:"); print $1","$2"\\n"}' > "$1.csv"
