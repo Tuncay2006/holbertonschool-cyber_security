3a30cf2ee2eadb0317f572536c1402a0ab17159f
Tuncay2006 <zenix@gmail.com>
2025-10-30 14:43:32 -0400

    dgfrz

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois -h whois.gandi.net "$1" | awk -F': ' '/^Registrant |^Admin |^Tech /{gsub(/^ +| +$/,"",$2); if($1~/Street$/)$2=$2" "; if($1~/Ext$/)sub(/Ext$/,"Ext:"); print $1","$2}' > "$1.csv"
+(cat "$1" 2>/dev/null || whois -h whois.gandi.net "$1" 2>/dev/null) | awk -F': ' '/^Registrant |^Admin |^Tech /{gsub(/^ +| +$/,"",$2); if($1~/Street$/)$2=$2" "; if($1~/Ext$/)sub(/Ext$/,"Ext:"); print $1","$2}' > "$1.csv"
