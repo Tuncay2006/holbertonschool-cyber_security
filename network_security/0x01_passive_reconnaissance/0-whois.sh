a183ba354593cbdf4ad2f9811906a2790006a7c3
Tuncay2006 <zenixthedark@gmail.com>
2025-11-01 13:53:45 +0400

    Update 0-whois.sh

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F: '/Registrant|Admin|Tech/ {gsub(/^[ \t]+/,"",$2); if($1~/Street/) $2=$2" "; if($1~/Ext$/) $1=$1":"; print $1","$2}'
+whois "$1" | awk -F: '/Registrant|Admin|Tech/ && !/Registry/ {gsub(/^[ \t]+|[ \t]+$/,"",$2); f=$1; gsub(/^[ \t]+|[ \t]+$/,"",f); if(f~/Street/) $2=$2" "; sub(/Ext$/,"Ext:",f); out=f","$2; lines[++n]=out} END{for(i=1;i<n;i++) print lines[i]; printf "%s", lines[n]}' | tee "$1.csv"
