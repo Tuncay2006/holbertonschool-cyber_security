2258d4f21d924ac2dbcb747b22b54cef90056217
Tuncay2006 <zenix@gmail.com>
2025-12-04 07:23:25 -0500

    fjgcd

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-grep -Ei "useradd|new user|added user" auth.log \
-| awk '{for(i=1;i<=NF;i++) if ($i=="user") print $(i+1)}' \
+grep -Ei "useradd|new user|added user|account created|creating user" auth.log \
+| grep -oE "[A-Za-z0-9_-]+" \
+| grep -Ev "useradd|user|new|added|account|created|creating|uid|gid|shell|sudo|by|for|from|session" \
 | sort -u \
 | paste -sd, -
