a64d09b1bde2d92ffea04b1611e8547a1ec33df8
Tuncay2006 <zenix@gmail.com>
2026-01-16 09:45:52 -0500

    fkgt

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,19 +1,18 @@
 # 4-write_file.rb
 require 'json'
-require_relative '3-read_file'
+require_relative '3-read_file'  # 3-read_file.rb'yi dahil et
 
 def merge_json_files(file1_path, file2_path)
-  # file1 ve file2'yi oku
+  # read_json_file fonksiyonunu kullan
   file1_data = read_json_file(file1_path)
   file2_data = read_json_file(file2_path)
 
-  # Merge işlemi
+  # Merge
   merged_data = if file1_data.is_a?(Array) && file2_data.is_a?(Array)
                   file2_data + file1_data
                 elsif file1_data.is_a?(Hash) && file2_data.is_a?(Hash)
                   file2_data.merge(file1_data)
                 else
-                  # Karışık tipler olursa flatten ile array haline getir
                   [file2_data, file1_data].flatten
                 end
 
@@ -21,6 +20,8 @@ def merge_json_files(file1_path, file2_path)
   File.open(file2_path, 'w') do |f|
     f.write(JSON.pretty_generate(merged_data))
   end
+
+  puts "Merged JSON written to #{file2_path}"
 end
 
 def count_user_ids(file_path)
