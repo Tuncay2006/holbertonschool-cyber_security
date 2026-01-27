702fa589423e2cf10c6556bf922a31e5ddea270f
Tuncay2006 <zenixthedark@gmail.com>
2026-01-27 19:53:46 +0400

    Update 4-write_file.rb

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,36 +1,14 @@
-# 4-write_file.rb
 require 'json'
-require_relative '3-read_file'  # 3-read_file.rb'yi dahil et
 
 def merge_json_files(file1_path, file2_path)
-  # read_json_file fonksiyonunu kullan
-  file1_data = read_json_file(file1_path)
-  file2_data = read_json_file(file2_path)
+  json1 = JSON.parse(File.read(file1_path))
+  json2 = JSON.parse(File.read(file2_path))
 
-  # Merge
-  merged_data = if file1_data.is_a?(Array) && file2_data.is_a?(Array)
-                  file2_data + file1_data
-                elsif file1_data.is_a?(Hash) && file2_data.is_a?(Hash)
-                  file2_data.merge(file1_data)
-                else
-                  [file2_data, file1_data].flatten
-                end
+  merged = json2 + json1
 
-  # file2.json'a yaz
   File.open(file2_path, 'w') do |f|
-    f.write(JSON.pretty_generate(merged_data))
+    f.write(JSON.pretty_generate(merged))
   end
 
   puts "Merged JSON written to #{file2_path}"
-end
-
-def count_user_ids(file_path)
-  data = read_json_file(file_path)
-  if data.is_a?(Array)
-    data.each_with_index do |item, index|
-      puts "#{index + 1}: #{item['userId']}"
-    end
-  else
-    puts "Dosya array değil, userId sayısı alınamıyor."
-  end
 end
